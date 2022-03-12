edb9a0369423c0e5f7c83abbe348687c4841999b
FlaggIgbo <theotherogeflagg@gmail.com>
2022-03-12 11:38:30 -0500

    11.38 commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let firstAnswer = window.prompt('Do you head left or right?')
+let firstAnswer = window.prompt('Do you head left or right? Those are your only options')
 if (firstAnswer.toLowerCase() === 'left') {
   let secondAnswer = window.prompt(`You come across a stray cat. It scampers off down a small hole, just large enough for you to crawl through. Do you follow it, or continue on your path?`)
   if (secondAnswer.toLowerCase() == 'follow') {
